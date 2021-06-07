626d593c3442101d7463b65dce3489bc1376addd
ChrizzieSFN <82824089+ChrizzieSFN@users.noreply.github.com>
2021-06-07 21:38:39 +0200

    semester selector added, styles etwas angepasst
    semester selector mti statischen daten angelegt, Hintergrundfarbe des Course Insighst Block und des next buttons in der Farbe von den Elas Buttons gechanged

--- src/components/Projects/CourseInsights/CourseInsights.js
+++ src/components/Projects/CourseInsights/CourseInsights.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, useHistory} from 'react-router-dom';
 import SearchBox from './SearchBoxStart/SearchBox';
 import WelcomeText from './WelcomeText/index'
+import {Selec, ButSelec} from './WelcomeText/WcTextElem'
 import {Button, Grid} from "@material-ui/core"; 
 import "./styles.css"
-
+import SemesterSelec from './SemesterSelec/SemesterSelec'
 
 
 export default function CourseInsights(){
@@ -18,21 +19,28 @@ export default function CourseInsights(){
     return (
       <Router>
         
-              <WelcomeText/> 
+        <WelcomeText/> 
 
+        <Selec>
 
-         <div style={{paddingTop:20}}> 
-        <SearchBox />   
-        </div>
+          <div style={{paddingTop:20}}> 
+            <SearchBox />   
+          </div>
+          <ButSelec>
+            <div style={{paddingTop:20}}>
+            <SemesterSelec/>
+            </div>
 
-        <div style={{paddingTop:20}}>  
-        <Button id = "next" 
+           <div style={{paddingTop:55, paddingLeft:30}}>  
+              <Button id = "next" 
                 variant="contained" 
-                color = "primary" 
+                style={{backgroundColor: "#3f51b5",color:"#fff", width: 70 }}
                 onClick={isLoggedIn ? () => history.push('/selectpage') : () => history.push('/login')}>
                 next 
-        </Button>
-        </div>
+              </Button>
+            </div>  
+          </ButSelec>
+        </Selec>
 
       </Router>
     );
